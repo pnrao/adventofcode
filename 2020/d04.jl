efd73173b82e5b0e4ce1aed71d845d40ef33ab68
prash <prash.n.rao@gmail.com>
2020-12-04 20:17:43 +0530

    minor cleanup

--- 2020/d04.jl
+++ 2020/d04.jl
@@ -21,16 +21,16 @@ for blk in db_blocks
         k,v = split(f, ":")
         if     k=="byr"
             n=parse(Int16,v)
-            if 1920 <= n <= 2002 byr=n end
+            if 1920 ≤ n ≤ 2002 byr=n end
         elseif k=="iyr"
             n=parse(Int16,v)
-            if 2010 <= n <= 2020 iyr=n end
+            if 2010 ≤ n ≤ 2020 iyr=n end
         elseif k=="eyr"
             n=parse(Int16,v)
-            if 2020 <= n <= 2030 eyr=n end
+            if 2020 ≤ n ≤ 2030 eyr=n end
         elseif k=="hgt"
-            if (endswith(v,"cm") && 150<=parse(Int,v[1:end-2])<=193) ||
-               (endswith(v,"in") && 59<=parse(Int,v[1:end-2])<=76)
+            if (endswith(v,"cm") && 150≤parse(Int,v[1:end-2])≤193) ||
+               (endswith(v,"in") && 59≤parse(Int,v[1:end-2])≤76)
                hgt=v end
         elseif k=="hcl"
             if length(v)==7 && occursin(r"#[0-9a-f]{6}", v)
@@ -47,13 +47,12 @@ for blk in db_blocks
         end
     end
     try
-        p = Passport(byr, iyr, eyr, hgt, hcl, ecl, pid, cid)
-        push!(pdb, p)
+        push!(pdb, Passport(byr, iyr, eyr, hgt, hcl, ecl, pid, cid))
     catch e
         if isa(e, UndefVarError)
             continue
         end
     end
 end
-#println(pdb)
+#println.(pdb)
 println(length(pdb))
