f253fe32f07d0baf452b0ff3b3849ec101d008cf
prash <prash.n.rao@gmail.com>
2020-12-06 15:07:42 +0530

    harmonization

--- 2020/d06.jl
+++ 2020/d06.jl
@@ -1,6 +1,6 @@
-blks = read("input06.txt",String)|>b->split(b, "\n\n")
-anyyes = filter.(isletter, blks).|>Set.|>length
+blks = read("input06.txt",String)|>b->split(b, "\n\n").|>split
+anyyes = blks.|>l->reduce(union,l)|>length
 println("Sum of questions to which anyone in each group answered \"yes\" = ", sum(anyyes))
 
-allyes = blks.|>split.|>l->reduce(intersect,l)|>length
+allyes = blks.|>l->reduce(intersect,l)|>length
 println("Sum of questions to which everyone in each group answered \"yes\" = ", sum(allyes))
