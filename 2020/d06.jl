25090281a83ef9b8126bd2ad52e5923129290ae0
prash <prash.n.rao@gmail.com>
2020-12-06 14:44:13 +0530

    minor

--- 2020/d06.jl
+++ 2020/d06.jl
@@ -1,6 +1,6 @@
 blks = read("input06.txt",String)|>b->split(b, "\n\n")
-allyes = filter.(isletter, blks).|>Set.|>length
-println("Sum of questions to which anyone in each group answered \"yes\" = ", sum(allyes))
-
-anyyes = blks.|>split.|>l->reduce(intersect,l)|>length
+anyyes = filter.(isletter, blks).|>Set.|>length
 println("Sum of questions to which anyone in each group answered \"yes\" = ", sum(anyyes))
+
+allyes = blks.|>split.|>l->reduce(intersect,l)|>length
+println("Sum of questions to which everyone in each group answered \"yes\" = ", sum(allyes))
