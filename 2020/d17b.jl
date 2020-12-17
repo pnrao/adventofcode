8dea82ba18169f9904de92f8dad6568dea8a00b6
prash <prash.n.rao@gmail.com>
2020-12-17 18:55:57 +0530

    day 17

---
+++ 2020/d17b.jl
@@ -0,0 +1,44 @@
+function makeinit(file)
+    str=read(file, String)
+    carts = Set{CartesianIndex{4}}()
+    for (y,s) in enumerate(split(str,"\n"))
+        for (x, c) in enumerate(s)
+            if c == '#'
+                push!(carts, CartesianIndex(x,y,0,0))
+            end
+        end
+    end
+    return carts
+end
+
+const unit=CartesianIndex(1,1,1,1)
+function nextextrema(arr)
+    e = extrema(arr)
+    return (e[1]-unit, e[2]+unit)
+end
+
+const window = setdiff(collect(CartesianIndex(-1, -1, -1, -1):
+                               CartesianIndex(1, 1, 1, 1)),
+                       [CartesianIndex(0, 0, 0, 0)])
+windowof(p::CartesianIndex)=Set(map(x->x+p, window))
+
+function next(active)
+    nextactive=Set{CartesianIndex{4}}()
+    ne = nextextrema(active)
+    for p ∈ ne[1]:ne[2]
+        actneighbrs = length(intersect(active, windowof(p)))
+        if p∈active && 2 ≤ actneighbrs ≤ 3
+            push!(nextactive, p)
+        elseif p∉active && actneighbrs == 3
+            push!(nextactive, p)
+        end
+    end
+    return nextactive
+end
+
+actives=makeinit("input17.txt")
+for i∈1:6
+    global actives=next(actives)
+end
+
+println(length(actives))
