dcf297886f5565a3238ddc0e794d44cb19bc8182
prash <prash.n.rao@gmail.com>
2020-12-21 10:50:12 +0530

    day 20 part 1 cleaned

--- 2020/d20p.jl
+++ 2020/d20p.jl
@@ -32,42 +32,36 @@ tiles = Dict()
 for grp in eachmatch(r"Tile (\d+):\n(.*?)\n\n"s,read("input20.txt", String))
 	n = parse(Int, grp[1])
 	m = replace(replace(grp[2],"#"=>" 1"),"."=>" 0")
-	try
 	m = eval(Meta.parse('['*m*']'))
-	catch
-		println(m)
-	end
 	tiles[n] = m
 end
 
 # ╔═╡ 93e878ec-42ab-11eb-242e-45544983869a
-edges = Dict()
-
-# ╔═╡ 9b464f1c-42ab-11eb-28ee-2dd7298941e8
-for k in keys(tiles)
-	edges[k]=intify(tiles[k])
-end
-
-# ╔═╡ f22da886-4298-11eb-0cd1-7fe1ab9c1a35
-nhbrs = Dict()
-
-# ╔═╡ a661a0ae-42ba-11eb-02ae-db732a0c5bb4
-for k in keys(edges)
-	nhbrs[k] = Set()
-end
-
-# ╔═╡ 41aa3f22-42b8-11eb-1f9e-9528af137ecb
-for j in keys(edges)
-	for k in keys(edges)
-		if j!=k && length(intersect(edges[j], edges[k]))>0
-			push!(nhbrs[j], k)
-			push!(nhbrs[k], j)
+function makenhbrs(ts)
+	edges = Dict()
+	for k in keys(ts)
+		edges[k]=intify(ts[k])
+	end
+	ns = Dict(k=>Set() for k in keys(ts))
+	for j in keys(edges)
+		for k in keys(edges)
+			if j!=k && length(intersect(edges[j], edges[k]))>0
+				push!(ns[j], k)
+				push!(ns[k], j)
+			end
 		end
 	end
+	return ns
 end
+
+# ╔═╡ 95f5bfc8-4348-11eb-368c-bf27e4a4a603
+nhbrs=makenhbrs(tiles)
 
 # ╔═╡ a4a4c518-42bd-11eb-08ec-03a10e6085a0
 cornerpieces = [k for k in keys(nhbrs) if length(nhbrs[k])==2]
+
+# ╔═╡ 46ee1140-42c7-11eb-0529-2504664840fa
+@show "Part 1: $(prod(cornerpieces))"
 
 # ╔═╡ Cell order:
 # ╠═754e855c-4299-11eb-321c-1f5b267e7f25
@@ -76,8 +70,6 @@ cornerpieces = [k for k in keys(nhbrs) if length(nhbrs[k])==2]
 # ╠═74971f6c-428e-11eb-31d3-0b5a3154b724
 # ╠═6b291190-42bd-11eb-3e35-35884a47923e
 # ╠═93e878ec-42ab-11eb-242e-45544983869a
-# ╠═9b464f1c-42ab-11eb-28ee-2dd7298941e8
-# ╠═f22da886-4298-11eb-0cd1-7fe1ab9c1a35
-# ╠═a661a0ae-42ba-11eb-02ae-db732a0c5bb4
-# ╠═41aa3f22-42b8-11eb-1f9e-9528af137ecb
+# ╠═95f5bfc8-4348-11eb-368c-bf27e4a4a603
 # ╠═a4a4c518-42bd-11eb-08ec-03a10e6085a0
+# ╠═46ee1140-42c7-11eb-0529-2504664840fa
