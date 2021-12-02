b5a3a8dfde9da87a67382dfd8f162f57861acf7a
prash <prash.n.rao@gmail.com>
2021-12-02 13:22:30 +0530

    preamble

---
+++ 2021/d01.jl
@@ -0,0 +1,30 @@
+include("preamble.jl")
+
+function readinput()
+	readlines("input01.txt").|>l->parse(Int,l)
+end
+
+global depths = readinput()
+
+@time function part1()
+	global depths
+	prev = depths[1]
+	incs = 0
+	for d in depths[2:end]
+		if d>prev incs +=1 end
+		prev = d
+	end
+	println("Part 1: ", incs)
+end
+
+@time function part2()
+	global depths
+	incs = 0
+	for i in 1:length(depths)-3
+		if depths[i]<depths[i+3] incs +=1 end
+	end
+	println("Part 2: ", incs)
+end
+
+part1()
+part2()
