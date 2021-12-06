b87722c4722fb045d6cea1b312bdd7f2b794101b
prash <prash.n.rao@gmail.com>
2021-12-06 12:15:50 +0530

    minor

--- 2021/d06.jl
+++ 2021/d06.jl
@@ -3,12 +3,12 @@ try include("preamble.jl") catch end
 sample =[3,4,3,1,2]
 
 function spawnfish(fish, days)
-	fishdic = Dict((i=>0 for i in 0:8))
-	for f in fish
+	fishdic = Dict((i=>0 for i ∈ 0:8))
+	for f ∈ fish
 		fishdic[f]+=1
 	end
 	for d ∈ 1:days
-		newdic = Dict((i=>0 for i in 0:8))
+		newdic = Dict((i=>0 for i ∈ 0:8))
 		for k ∈ keys(fishdic)
 			if k==0
 				newdic[6]+=fishdic[0]
@@ -27,7 +27,7 @@ input = parse.(Int,split(readline("input06.txt"),','))
 @show sample18
 @time sample80 = spawnfish(sample,80)
 @show sample80
-@time part1=spawnfish(copy(input), 80)
+@time part1=spawnfish(input, 80)
 @show part1
 @time part2=spawnfish(input, 256)
 @show part2
